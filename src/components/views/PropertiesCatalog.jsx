9bef61df3e80741b1ab57cd559052091cdcfac05
marioa98 <mmartinez53@ucol.mx>
2020-08-05 13:43:54 -0500

    [FIX] Hotfix in Pagination Component

--- src/components/views/PropertiesCatalog.jsx
+++ src/components/views/PropertiesCatalog.jsx
@@ -34,7 +34,7 @@ export default function PropertiesCatalog(props){
 
   const PaginatorProps = {
     totalPages: totalPages,
-    activePage: page,
+    currentPage: page,
     handleChange: handleChange
   }
 
