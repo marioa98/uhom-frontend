32d10631c92454e22fcdd2c52d5544aea1274e8f
marioa98 <mmartinez53@ucol.mx>
2020-08-24 14:21:17 -0500

    [REF] User avatar in navbar

--- src/components/smart/Navbar/Avatar.jsx
+++ src/components/smart/Navbar/Avatar.jsx
@@ -1,6 +1,6 @@
 import React from "react"
-import { Dropdown} from "semantic-ui-react";
-import { useHistory } from "react-router-dom"
+import { Dropdown, Image } from "semantic-ui-react";
+import { Redirect, useHistory } from "react-router-dom"
 import "../../../assets/styles/General/icons.css"
 import { logoutHandler } from "../../../services/sessionHandlers/authService";
 import { useSessionInfo } from "../../../services/sessionInfo"
@@ -11,29 +11,45 @@ export function Avatar(){
   const dispatch = useUserDispatch();
   const history = useHistory();
 
+  const trigger = (
+    <span>
+      <Image 
+        avatar 
+        src="https://en.gravatar.com/userimage/191265949/d6b5b62c51acb05d460a4b6f39610410.png?size=100"
+        size="mini"
+      />
+    </span>
+  )
+
+  const pathAfterLogout = () => {
+    const pathRegexp = /\/user/
+    const currentPath = history.location.pathname
+    return pathRegexp.test(currentPath) ? '/' : currentPath
+  }
+
   const logout = () => {
     logoutHandler(session.authorization, dispatch);
+    history.push(pathAfterLogout())
   }
 
   const goTo = section => {
-    history.replace(`/user/${session.id}/${section}`)
+    history.push(`/user/${session.id}/${section}`)
   }
 
   
   return(
     <div>
       <Dropdown
-        className='icon'
-        direction="left"
-        pointing="top"
-        icon={{name: 'user circle', color: "blue", size: "big"}}
+        trigger={trigger}
+        pointing="top right"
+        icon={null}
       > 
         <Dropdown.Menu>
           <Dropdown.Header content={`¡Hola, ${ session.names }! `}/>
 
           <Dropdown.Divider />
           
-          <Dropdown.Item icon='user' text='Mi Perfil' onClick={() => goTo('my-profile')}/>
+          <Dropdown.Item icon='user' text='Mi Perfil' onClick={() => goTo('info')}/>
           <Dropdown.Item icon='heart' text='Mis Casas' onClick={() => goTo('my-likes')}/>
           
           <Dropdown.Divider />
