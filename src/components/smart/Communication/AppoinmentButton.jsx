86133f7578a9eee58adbe9e3dbe35a7e14371035
marioa98 <mmartinez53@ucol.mx>
2020-08-26 18:15:31 -0500

    [ADD] Show modal in appointment when is not logged

--- src/components/smart/Communication/AppoinmentButton.jsx
+++ src/components/smart/Communication/AppoinmentButton.jsx
@@ -1,9 +1,13 @@
-import React from "react";
-import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useUserContext } from "../../../UserContext"
 import "../../../assets/styles/General/divs.css";
+import SigninFirst from "../Modals/SigninFirst";
 
 export default function AppopinmentButton(props){
   const {address, price} = props
+  const [modalOpen, handleModal] = useState(false)
+  const { isLogged } = useUserContext();
   const baseUri = "https://api.whatsapp.com/send?"
   const defaultPhone = "5213121042284"
 
@@ -16,11 +20,22 @@ export default function AppopinmentButton(props){
     return `Hola, me gustaría una cita para conocer la casa ubicada en ${address}, que tiene un costo publicado de ${price}. Saludos!`
   }
 
+  const setModal = () => handleModal(!modalOpen)
+  const handleAppoinment = () => isLogged ? sendMessage() : setModal()
+
   return(
-    <div className="fluid">
-      <Link className="ui button btn-signin" onClick={() => sendMessage()}>
-        ¡Haz una cita, visita la casa!
-      </Link>
+    <div>
+      <div>
+        <SigninFirst
+          isOpen={modalOpen}
+          handleModal={setModal}
+        />
+      </div>
+      <div className="fluid">
+        <Link className="ui button btn-signin" onClick={() => handleAppoinment()}>
+          ¡Haz una cita, visita la casa!
+        </Link>
+      </div>
     </div>
   )
 }
