4f6ad11429a8f9a3795699edbe0f19ef77c4f085
marioa98 <mmartinez53@ucol.mx>
2020-09-01 20:58:55 -0500

    [FIX] Footer at page bottom

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from "react";
 import "./assets/styles/App.css";
+import "./assets/styles/General/divs.css";
 
 import Navbar from "./components/smart/Navbar/Navbar";
 import Routes from './routes/Routes'
 import Footer from './components/dumb/Footer'
 import { BrowserRouter } from "react-router-dom";
 import { UserContextProvider } from "./UserContext";
-import { Grid } from "semantic-ui-react";
 
 function App() {
   return (
     <BrowserRouter>
       <UserContextProvider>
         <Navbar>
-          <Routes/>
-          <Footer/>
+          <div className="body-container">
+            <div className="content">
+              <Routes/>
+            </div>                
+            <Footer/>
+          </div>
         </Navbar>
       </UserContextProvider>
     </BrowserRouter>
