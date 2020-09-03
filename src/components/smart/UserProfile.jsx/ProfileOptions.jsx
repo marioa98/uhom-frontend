7297ee7cf46cd1d4aefd30d43ed91c6ff33fbd50
marioa98 <mmartinez53@ucol.mx>
2020-09-02 20:35:53 -0500

    [ADD] Hook to format CRLF in text

--- src/components/smart/UserProfile.jsx/ProfileOptions.jsx
+++ src/components/smart/UserProfile.jsx/ProfileOptions.jsx
@@ -1,31 +1,9 @@
 import React from "react";
 import { Tab } from "semantic-ui-react";
-import ProfileInfo from "./ProfileInfo";
+import { panes } from "../../../assets/resources/userProfileOptions"
 
 export function ProfileOptions(props){
   const { activeIndex, handleSectionChange } = props
-  const panes = [
-    {
-      menuItem: 'Mis Casas',
-      pane: {
-        key: 'my-likes',
-        content:(
-          <Tab.Pane>Mis Casas</Tab.Pane>
-        )
-      }
-    },
-    {
-      menuItem: 'Información',
-      pane: {
-        key: 'info',
-        content:(
-          <Tab.Pane>
-            <ProfileInfo />
-          </Tab.Pane>
-        )
-      }
-    }
-  ]
 
   return(
     <Tab 
