c0d92d42fc06b0ac8664a8a456bb9e025a63f590
marioa98 <mmartinez53@ucol.mx>
2020-09-02 20:34:49 -0500

    [DEL] gender options

--- src/assets/resources/genders.js
+++
@@ -1,5 +0,0 @@
-export const genderOptions = [
-  {key: "not_defined", value: 0, text: "Prefiero no decirlo"},
-  {key: "female", value: 1, text: "Mujer"},
-  {key: "male", value: 2, text: "Hombre"}
-]
