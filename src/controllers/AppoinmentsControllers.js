ea3aa798b67caf0f057cd0ee5a8e461ea9a288ed
marioa98 <mmartinez53@ucol.mx>
2020-09-14 16:57:54 -0500

    refactor: delete unused components

--- src/controllers/AppoinmentsControllers.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const baseUri = "https://api.whatsapp.com/send?"
-const defaultPhone = "5213121042284"
-export default class AppoinmentsController {
-  static send(message){
-    return axios.post(`${baseUri}phone=${defaultPhone}&text=${message}`)
-  }
-}
