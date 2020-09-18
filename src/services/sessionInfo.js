ea3a660d2c8b60a88a532b650befed44702b84d4
marioa98 <mmartinez53@ucol.mx>
2020-09-18 18:33:20 -0500

    refactor: Delete unused stuff

--- src/services/sessionInfo.js
+++ src/services/sessionInfo.js
@@ -1,10 +1,4 @@
-import { useEffect, useState } from "react";
-import { useUserContext } from "../UserContext";
-import { setSessionItem } from "./sessionHandlers/localStorageHandler";
-
 export function useSessionInfo(){
   const session = JSON.parse(localStorage.getItem('session'));
   return session
 }
-
-const getSessionItem = () => JSON.parse(localStorage.getItem('session'))
